26ddb56f833fabc51c4bb4022e57d109b64e11ae
Dom Leone <ohheydom@gmail.com>
2014-05-23 16:37:41 -0400

    Buttons become disabled once clicked.

--- app/helpers/homes_helper.rb
+++ app/helpers/homes_helper.rb
@@ -1,25 +1,26 @@
 module HomesHelper
   def isdisabled(box, game)
     if game.turn == 'o'
-      true                 
-    else                   
+      true
+    else
       if game.win?(@game.previous_player) == true
         true
       else
-        box == '-' ? false : true       
+        box == '-' ? false : true
       end
     end
   end
 
-  def tic_tac_toe_row(game, row)  
+  def tic_tac_toe_row(game, row)
     rows = { one: [*0..2], two: [*3..5], three: [*6..8] }
     game.board.each_with_index.inject("<div id='row_#{row}'>") do |acc, (box, ind)|
-      sub_tag = button_to box, { action: 'update_game_board', id: "button_#{ind}" }, disabled: false
-      if rows[row].include?(ind)      
+      disabled = game.board[ind] == '-' ? false : true
+      sub_tag = button_to box, { action: 'update_game_board', id: "button_#{ind}" }, disabled: disabled
+      if rows[row].include?(ind)
         acc << content_tag(:div, sub_tag, class: "grid box_#{ind}")
-      else                 
-        acc << ''          
-      end                  
-    end.html_safe          
-  end                      
+      else
+        acc << ''
+      end
+    end.html_safe
+  end
 end
