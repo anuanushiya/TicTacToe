cd3a728c18603ccf70a7b054a4e7a30675b4b0a9
Dom Leone <ohheydom@gmail.com>
2014-05-24 01:55:59 -0400

    Moved some methods from tic_tac_toe to computer_ai.

--- lib/tic_tac_toe/computer_ai.rb
+++ lib/tic_tac_toe/computer_ai.rb
@@ -1,9 +1,50 @@
 class ComputerAI
-  def initialize(game_board, turn)
+  def initialize(game_board, turn, check_winner)
     @game_board = game_board
     @turn = turn
+    @check_winner = check_winner
   end
 
-  def move
+  def human_player
+    @turn == 'o' ? 'x' : 'o'
+  end
+
+  def best_move
+    @old_board = @game_board.board.dup
+    computer_win(@old_board) || stop_human_win(@old_board) || play_center || play_corner || play_side
+  end
+
+  private
+
+  def computer_win(old_board)
+    winning_move = @game_board.remaining_indices.select do |move| 
+      @game_board.move(move, 'o')
+      win = @check_winner.new(@game_board.board, @turn).win?
+      @game_board.board = old_board.dup
+      win
+    end
+    winning_move.empty? ? nil : winning_move
+  end
+
+  def stop_human_win(old_board)
+    stop_human_move = @game_board.remaining_indices.select do |move| 
+      @game_board.move(move, 'x')
+      win = @check_winner.new(@game_board.board, human_player).win?
+      @game_board.board = old_board.dup
+      win
+    end
+    stop_human_move.empty? ? nil : stop_human_move
+  end
+
+  def play_center
+    @game_board.move(4, 'o') ? [4] : nil
+  end
+
+  def play_corner
+    @game_board.move(0, 'o') || @game_board.move(2, 'o') || @game_board.move(6, 'o') || @game_board.move(8, 'o')
+  end
+
+  def play_side
+    @game_board.move(1, 'o') || @game_board.move(3, 'o') || @game_board.move(5, 'o') || game_board.move(7, 'o')
   end
 end
--- lib/tic_tac_toe/tic_tac_toe_game.rb
+++ lib/tic_tac_toe/tic_tac_toe_game.rb
@@ -32,41 +32,6 @@ class TicTacToeGame
   end
 
   def computer_move
-    @old_board = board.dup
-    computer_win(@old_board) || stop_human_win(@old_board) || play_center || play_corner || play_side
-  end
-
-  private
-
-  def computer_win(old_board)
-    winning_move = @game_board.remaining_indices.select do |move| 
-      @game_board.move(move, 'o')
-      win = win?('o')
-      @game_board.board = old_board.dup
-      win
-    end
-    winning_move.empty? ? nil : winning_move
-  end
-
-  def stop_human_win(old_board)
-    stop_human_move = @game_board.remaining_indices.select do |move| 
-      @game_board.move(move, 'x')
-      win = win?('x')
-      @game_board.board = old_board.dup
-      win
-    end
-    stop_human_move.empty? ? nil : stop_human_move
-  end
-
-  def play_center
-    @game_board.move(4, 'o') ? [4] : nil
-  end
-
-  def play_corner
-    @game_board.move(0, 'o') || @game_board.move(2, 'o') || @game_board.move(6, 'o') || @game_board.move(8, 'o')
-  end
-
-  def play_side
-    @game_board.move(1, 'o') || @game_board.move(3, 'o') || @game_board.move(5, 'o') || game_board.move(7, 'o')
+    @computer_ai.new(@game_board, 'o', @check_winner).best_move
   end
 end
