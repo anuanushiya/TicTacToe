9a73c1f7087d1f8d593f1c045a81ee404fa78678
Dom Leone <ohheydom@gmail.com>
2014-05-24 08:47:20 -0400

    Computer player moves.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,13 @@
 class HomesController < ApplicationController
+  after_action :computer_move
+
   def show
+  end
+
+  def computer_move
+    if GAME.current_player == 'o'
+      GAME.move(GAME.computer_move.first)
+    end
   end
 
   def update_game_board
