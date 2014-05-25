d48ee50d18d80553a43fa43be5bf82de0eb19b89
Dom Leone <ohheydom@gmail.com>
2014-05-25 13:05:01 -0400

    Refactored computer_ai class.

--- lib/tic_tac_toe/computer_ai.rb
+++ lib/tic_tac_toe/computer_ai.rb
@@ -1,4 +1,5 @@
 class ComputerAI
+  CORNERS = [0, 2, 6, 8]
   def initialize(game_board, turn, check_winner)
     @game_board = game_board
     @old_board = game_board.board.dup
@@ -11,31 +12,30 @@ class ComputerAI
   end
 
   def best_move
-    if @game_board.board[0] == human_player || @game_board.board[2] == human_player ||
-      @game_board.board[6] == human_player || @game_board.board[8] == human_player
-      computer_win || stop_human_win || play_center || play_side || play_corner || 0
+    if CORNERS.map { |ind| @game_board.board[ind] }.any? { |square| square == human_player }
+      play_to_win || play_to_stop_human_win || play_center || play_side || play_corner || 0
     else
-      computer_win || stop_human_win || play_center || play_corner || play_side || 0
+      play_to_win || play_to_stop_human_win || play_center || play_corner || play_side || 0
     end
   end
 
   private
 
-  def computer_win
+  def play_to_win
     winning_move = @game_board.remaining_indices.select do |move|
       @game_board.move(move, @turn)
       win = @check_winner.new(@game_board.board, @turn).win?
-      return_board
+      rollback_board
       win
     end
     winning_move.empty? ? nil : winning_move.first
   end
 
-  def stop_human_win
+  def play_to_stop_human_win
     stop_human_move = @game_board.remaining_indices.select do |move|
       @game_board.move(move, human_player)
       win = @check_winner.new(@game_board.board, human_player).win?
-      return_board
+      rollback_board
       win
     end
     stop_human_move.empty? ? nil : stop_human_move.first
@@ -43,23 +43,23 @@ class ComputerAI
 
   def play_center
     move = @game_board.move(4, @turn) ? 4 : nil
-    return_board
+    rollback_board
     move
   end
 
   def play_corner
     move = @game_board.move(0, @turn) || @game_board.move(2, @turn) || @game_board.move(6, @turn) || @game_board.move(8, @turn)
-    return_board
+    rollback_board
     move
   end
 
   def play_side
     move = @game_board.move(1, @turn) || @game_board.move(3, @turn) || @game_board.move(5, @turn) || @game_board.move(7, @turn)
-    return_board
+    rollback_board
     move
   end
 
-  def return_board
+  def rollback_board
     @game_board.board = @old_board.dup
   end
 end
