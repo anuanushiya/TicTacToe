485eb2e38b08287257d248d15b41bb05b8ccaf69
Dom Leone <ohheydom@gmail.com>
2014-05-27 01:11:32 -0400

    Added another jasmine spec.

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -6,11 +6,15 @@ function clickButton(button) {
   setTimeout(function() {button.click()}, 1);
 }
 
+function computer_play(button) {
+  if ($('#turn').val() == 'o' && $('h2.winner').text().length == 0) {
+    removeDisabled(button);
+    clickButton(button);
+  }
+};
+
 $(document).ready(function() {
   var computer_move = $('#computer_move').val();
   var computer_button = $(String.concat('.box_', computer_move, " input[type='submit']"));
-  if ($('#turn').val() == 'o' && $('h2.winner').text().length == 0) {
-    removeDisabled(computer_button);
-    clickButton(computer_button);
-  }
+  computer_play(computer_button);
 });
--- spec/javascripts/homes_spec.js
+++ spec/javascripts/homes_spec.js
@@ -1,23 +1,31 @@
 describe('homes', function(){
   beforeEach(function() {
-    timerCallback = jasmine.createSpy("timerCallback");
+    loadFixtures('game_board.html');
     jasmine.clock().install();
   });
 
-  it('removes disabled from a button', function() {
-    loadFixtures('game_board.html');
-    var button_4 = $(".box_4 input[type='submit']")
-    removeDisabled(button_4);
-    expect(button_4).not.toBeDisabled();
+  it('removes disabled attribute from a button', function() {
+    var computer_move = $('#computer_move').val();
+    var button = $(String.concat(".box_", computer_move, " input[type='submit']"));
+    removeDisabled(button);
+    expect(button).not.toBeDisabled();
   });
 
   it('clicks on the link indicated by hidden_field computer_move', function() {
-    loadFixtures('game_board.html');
     var computer_move = $('#computer_move').val();
     var button = $(String.concat(".box_", computer_move, " input[type='submit']"));
     spyOnEvent(button, 'click');
     clickButton(button);
-    jasmine.clock().tick(10);
+    jasmine.clock().tick(1);
+    expect('click').toHaveBeenTriggeredOn(button);
+  });
+
+  it('plays the computer move when page is loaded', function() {
+    var computer_move = $('#computer_move').val();
+    var button = $(String.concat(".box_", computer_move, " input[type='submit']"));
+    spyOnEvent(button, 'click');
+    computer_play(button);
+    jasmine.clock().tick(1);
     expect('click').toHaveBeenTriggeredOn(button);
   });
 });
