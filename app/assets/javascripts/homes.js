18bf1092fda7a87a0d9766ae824d1da1db8b31bb
Dom Leone <ohheydom@gmail.com>
2014-05-27 00:56:01 -0400

    Fixed up jasmine specs.

--- app/assets/javascripts/homes.js
+++ app/assets/javascripts/homes.js
@@ -1,16 +1,16 @@
-function remove_disabled(button) {
+function removeDisabled(button) {
   button.removeAttr('disabled');
 }
 
-function click_button(button) {
-  setTimeout(function() {$(button).click()}, 1);
+function clickButton(button) {
+  setTimeout(function() {button.click()}, 1);
 }
 
 $(document).ready(function() {
   var computer_move = $('#computer_move').val();
   var computer_button = $(String.concat('.box_', computer_move, " input[type='submit']"));
   if ($('#turn').val() == 'o' && $('h2.winner').text().length == 0) {
-    remove_disabled(computer_button);
-    click_button(computer_button);
+    removeDisabled(computer_button);
+    clickButton(computer_button);
   }
 });
--- spec/javascripts/homes_spec.js
+++ spec/javascripts/homes_spec.js
@@ -1,7 +1,23 @@
 describe('homes', function(){
-  loadFixtures('game_board.html');
+  beforeEach(function() {
+    timerCallback = jasmine.createSpy("timerCallback");
+    jasmine.clock().install();
+  });
+
   it('removes disabled from a button', function() {
-    $(".box_4 input[type='submit']").attr('disabled', 'disabled');
-    expect(remove_disabled($(".box_4 input[type='submit']"))).not.toBeDisabled();
+    loadFixtures('game_board.html');
+    var button_4 = $(".box_4 input[type='submit']")
+    removeDisabled(button_4);
+    expect(button_4).not.toBeDisabled();
+  });
+
+  it('clicks on the link indicated by hidden_field computer_move', function() {
+    loadFixtures('game_board.html');
+    var computer_move = $('#computer_move').val();
+    var button = $(String.concat(".box_", computer_move, " input[type='submit']"));
+    spyOnEvent(button, 'click');
+    clickButton(button);
+    jasmine.clock().tick(10);
+    expect('click').toHaveBeenTriggeredOn(button);
   });
 });
